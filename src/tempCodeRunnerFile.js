8a90dc58b00c17a1b9c042bd9451fe0039da5f48
77Monisha <monishachaurasia77@gmail.com>
2022-08-14 20:37:48 +0530

    initial commit

---
+++ src/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+ exact path="C:\Users\Aryan\Desktop\folders\college\web\React tutorial\my-app\src\components">
+            <About mode= {mode}/>
+          </Route>
+          <Route exact path="/">
+            <TextForm showAlert={showAlert} heading="Try Texty - Word and Character Counter, Trim irrelevent spaces!" mode={mode}/>
+          </Rout
